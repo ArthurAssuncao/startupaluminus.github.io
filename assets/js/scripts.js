10b58aa95d173caaf7f1da0944e48c8c64776882
ArthurAssuncao <arthur.xx92@gmail.com>
2016-02-01 15:30:49 -0200

    changed: navbar e efeitos na navbar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,15 @@
+function scrollspy(){
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+        $('#nav_f').addClass('nav-shrink');
+        console.log("add nav-shrink");
+      } else {
+        $('#nav_f').removeClass('nav-shrink');
+        console.log("rm nav-shrink");
+      }
+    });
+}
+
 $(document).ready(function(){
-    
+    scrollspy();
 });
